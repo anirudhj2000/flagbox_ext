28e7dcf10f0014f37028087a4a2bffefb3244ba8
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-08-01 22:10:27 +0530

    webpack  changes

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
 
-let htmlPageNames = ["buttoncomponent"];
+let htmlPageNames = ["buttoncomponent", "loginscreen"];
 let multipleHtmlPlugins = htmlPageNames.map((name) => {
   return new HtmlWebpackPlugin({
-    template: `./src/${name}.html`, // relative path to the HTML files
+    template: `./src/${name}/${name}.html`, // relative path to the HTML files
     filename: `${name}.html`, // output HTML files
     chunks: [`${name}`], // respective JS files
   });
@@ -18,7 +20,8 @@ module.exports = {
     contentScript: "./src/content/index.ts",
     background: "./src/background/index.ts",
     react: "./src/react/index.tsx",
-    buttoncomponent: "./src/buttoncomponent.ts",
+    buttoncomponent: "./src/buttoncomponent/buttoncomponent.ts",
+    loginscreen: "./src/loginscreen/loginscreen.ts",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -34,6 +37,10 @@ module.exports = {
       patterns: [
         { from: path.resolve("manifest.json"), to: path.resolve("dist") },
         { from: path.resolve("src", "index.css"), to: path.resolve("dist") },
+        {
+          from: path.resolve("src", "loginscreen", "loginscreen.css"),
+          to: path.resolve("dist"),
+        },
       ],
     }),
   ].concat(multipleHtmlPlugins),
@@ -52,6 +59,22 @@ module.exports = {
             ],
           },
         },
+      },
+      {
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                indent: "postcss",
+                plugins: [tailwindcss, autoprefixer],
+              },
+            },
+          },
+        ],
+        test: /\.css$/i,
       },
     ],
   },
