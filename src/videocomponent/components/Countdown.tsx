9d20d9a14a22250877d76ea60f7d17ecc6fa60f9
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-30 01:40:18 +0530

    video component changes

---
+++ src/videocomponent/components/Countdown.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+interface CountdownProps {
+    initialCount: number
+    onCountEnd: () => void
+}
+
+const Countdown = ({
+    initialCount = 5,
+    onCountEnd
+}: CountdownProps) => {
+    const [count, setCount] = useState(initialCount)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount(count - 1)
+        }, 1000)
+
+        if (count === 0) {
+            clearInterval(interval)
+            onCountEnd && onCountEnd()
+        }
+
+        return () => clearInterval(interval)
+    }, [count])
+
+    return (
+        <div className='text-red-600 text-[4rem] font-bold '>{count}</div>
+    )
+}
+
+export default Countdown
