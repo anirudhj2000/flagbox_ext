e7a5f2c8e8e46ffc158bb1c975024b8a1b9f1938
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-10-30 17:05:51 +0530

    app changes

--- src/videocomponent/app.tsx
+++ src/videocomponent/app.tsx
@@ -17,9 +17,14 @@ const App = () => {
   const [showCountdown, setShowCountdown] = useState(true);
 
   const startRecording = () => {
-    chrome.runtime.sendMessage({ type: "start_recording" });
+    chrome.runtime.sendMessage({ command: "startRecording" });
     // window.parent.postMessage({ type: "start_recording" }, "*");
     setShowCountdown(false);
+  };
+
+  const stopRecording = () => {
+    chrome.runtime.sendMessage({ command: "stopRecording" });
+    // window.parent.postMessage({ type: "stop_recording" }, "*");
   };
 
   return (
@@ -27,11 +32,9 @@ const App = () => {
       {showCountdown ?
         <div className="w-[40vh] h-[40vh] rounded-full flex flex-col shadow-2xl items-center justify-center bg-white">
           <p className=" text-xl text-red-400 font-bold">Recording will start in</p>
-          <Countdown initialCount={2} onCountEnd={() => {
-            startRecording();
-          }} />
+          <Countdown initialCount={2} onCountEnd={startRecording} />
         </div> :
-        <button onClick={() => { chrome.runtime.sendMessage({ type: "stop_recording" }) }} className=" px-4 py-2 rounded-full flex flex-row items-center bg-red-500 text-white">
+        <button onClick={stopRecording} className=" px-4 py-2 rounded-full flex flex-row items-center bg-red-500 text-white">
           <FaStopCircle />
           <p className="ml-2">Stop Recording</p>
         </button>
